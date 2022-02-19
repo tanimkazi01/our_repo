70f1685f2d73fdc2f9c6d095f10f2e3c60fbe539
TOUSHIF HOSSAIN <68136526+toushifhossain1@users.noreply.github.com>
2022-02-20 04:31:49 +0600

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,21 @@
+input = input("Enter a 3 digit Number: ")
+input=int(input)
+copyInput=int(input)
+
+num1 = input%10 
+input = input//10                 
+num2= input%10
+input= input//10
+
+num3=input
+
+cube=(num1*num1*num1)+(num2*num2*num2)+(num3*num3*num3)
+
+
+
+if cube==copyInput: 
+    print("Armstrong")
+
+else:
+    print("Not Armstrong")
+
